43794ec76c11ea5688d93b91884cef2b408f0bef
Hanif Fakira <fakira.work@gmail.com>
2017-12-23 11:41:28 +0000

    Refactored using reverse method

--- src/main/java/com/hanfak/PalindromeFinder.java
+++ src/main/java/com/hanfak/PalindromeFinder.java
@@ -1,18 +1,12 @@
 package com.hanfak;
 
+import java.util.*;
+
 class PalindromeFinder {
-    public boolean evaluate(String text) {
-        return text.length() < 2 || endsMatch(text) && evaluate(innerText(text));
-    }
-
-    private boolean endsMatch(String text) {
-        int indexOfLastChar = text.length() - 1;
-        char firstChar = text.charAt(0);
-        char lastChar = text.charAt(indexOfLastChar);
-        return firstChar == lastChar;
-    }
-
-    private String innerText(String text) {
-        return text.substring(1, text.length() - 1);
+    boolean evaluate(String text) {
+        List<String> textArrayList = Arrays.asList(text.split(""));
+        List<String> reversedArrrayList = new ArrayList<>(textArrayList);
+        Collections.reverse(reversedArrrayList);
+        return String.join("", textArrayList).equals(String.join("", reversedArrrayList));
     }
 }
