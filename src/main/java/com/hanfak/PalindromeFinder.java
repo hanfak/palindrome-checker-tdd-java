3eb8f3c4e8561f96bd7889917a6ac041c97c39dd
Hanif Fakira <fakira.work@gmail.com>
2017-12-23 11:46:49 +0000

    Refactored using reverse method from string builder, no converting to arrays and back to string

--- src/main/java/com/hanfak/PalindromeFinder.java
+++ src/main/java/com/hanfak/PalindromeFinder.java
@@ -1,12 +1,7 @@
 package com.hanfak;
 
-import java.util.*;
-
 class PalindromeFinder {
     boolean evaluate(String text) {
-        List<String> textArrayList = Arrays.asList(text.split(""));
-        List<String> reversedArrrayList = new ArrayList<>(textArrayList);
-        Collections.reverse(reversedArrrayList);
-        return String.join("", textArrayList).equals(String.join("", reversedArrrayList));
+        return new StringBuilder(text).reverse().toString().equals(text);
     }
 }
